c47ba388c3032ec5a935807fae272b2a784433fa
Sara Herlet <Sara.Herlet@gmail.com>
2020-05-11 22:58:34 +0200

    First working draft of looping text

--- script.js
+++ script.js
@@ -1,35 +1,44 @@
 // JavaScript Document
-
-
- /* var typing = new Typed('#typing', {
-    strings: ['Some <i>strings</i> with', 'Some <strong>HTML</strong>', 'Chars &times; &copy;'],
-    typeSpeed: 0,
-    backSpeed: 0,
-    fadeOut: true,
-    loop: true
-  }); */
-
- 
 var i = 0;
 var txt = 'For artists by artists'; /* The text */
+var txt1 = 'Get started!';
 var speed = 50; /* The speed/duration of the effect in milliseconds */ 
+var switching = 0;
+
+function loopingText(){
+		//typeWriter();
+		setTimeout(typeWriter, 0);
+		setTimeout(typeDelete, 2000);
+		setTimeout(typeWriter1, 2000);
+		setTimeout(loopingIt, 5000);
+	} 
 
 function typeWriter() {
   if (i < txt.length) {
     document.getElementById("demo").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
-  }
+  } 
 }
-	
+
+function typeWriter1() {
+  if (i < txt1.length) {
+    document.getElementById("demo").innerHTML += txt1.charAt(i);
+    i++;
+    setTimeout(typeWriter1, speed);
+  } 
+}
+
 function typeDelete() {
+document.getElementById("demo").innerHTML = "";
 	i=0;
-  if (i < document.getElementById("demo").innerHTML.length) {
-    document.getElementById("demo").innerHTML = "";
   }
 	
-
-
-	
-	
+function loopingIt(){
+	document.getElementById("demo").innerHTML = "";
+	i=0;
+	loopingText();
 }
+
+	
+	
