8ed0b3a8724a7a58dc1e86bd166daf1c8a7fc50f
Malene Madsen <malene.t.madsen@hotmail.com>
2020-05-18 18:51:29 +0200

    Javascript addet

--- script.js
+++ script.js
@@ -56,3 +56,100 @@ function switchStyle() {
     document.getElementById('exampleModal').classList.remove("custom");
   }
 }
+
+
+
+
+
+
+/*bestil*/
+function validate(){
+  var order=8; // Der kan max fratrækkes 8, derfor er denne værdi sat til 8
+  var petname = document.getElementById("petname").value;
+  var pet = document.getElementById("pet").value;
+  var race = document.getElementById("race").value;
+  var name = document.getElementById("name").value;
+  var adresse = document.getElementById("adresse").value;
+  var postnumber = document.getElementById("postnumber").value;
+  var email = document.getElementById("email").value;
+  var phone = document.getElementById("phone").value;
+  // var message = document.getElementById("messagecontact").value;
+  var error_message = document.getElementById("error_message");
+  var error_message1 = document.getElementById("error_message1");
+
+  var text;
+  if(petname.length < 1){ // Dette if-statement tjekker om kæledyrsnavnet er mindre end 1
+    text = "Intast dit kæledyrs navn"; // sætter teksten til det valgte
+    error_message.innerHTML = text; // indsætter teksten i HTML
+	error_message.className = 'alert-danger mb-1'; // Den indsatte tekst bliver rød
+	order--; // mangler udfyldt felt medfører en fratrækning fra order.
+	return false; 
+	}
+	
+  if(pet.length < 3){
+    text = "Intast kæledyr";
+    error_message.innerHTML = text;
+	error_message.className = 'alert-danger mb-1';
+	order--;
+	return false;
+  }
+
+	if(race.length < 4){
+    text = "Intast dit kæledyrs race";
+    error_message.innerHTML = text;
+	error_message.className = 'alert-danger mb-1';
+	order--;
+	return false;
+	}
+
+	if(name.length < 6){
+    text = "Intast dit dit fulde navn";
+    error_message1.innerHTML = text;
+	error_message1.className = 'alert-danger mb-1';
+	order--;
+	return false;
+	}
+
+	if(adresse.length < 6){
+    text = "Intast din adresse";
+    error_message1.innerHTML = text;
+	error_message1.className = 'alert-danger mb-1';
+	order--;
+	return false;
+	}
+
+	if(postnumber.length != 4 || isNaN(postnumber)){
+    text = "Intast dit postnummer";
+    error_message1.innerHTML = text;
+	error_message1.className = 'alert-danger mb-1';
+	order--;
+	return false;
+	}
+
+	if(email.indexOf("@") == -1 || email.length < 6){
+    text = "Indtast email";
+    error_message1.innerHTML = text;
+	error_message1.className = 'alert-danger mb-1';
+	order--;
+	return false;
+	}
+
+	if(isNaN(phone) || phone.length != 8){
+    text = "intast mobilnummer";
+    error_message1.innerHTML = text;
+	error_message1.className = 'alert-danger mb-1';
+	order--;
+	return false;
+	}
+
+	/*if(message.length <= 0 || message.length == null){
+	error_message1.innerHTML = text;
+	error_message1.className = 'alert-danger mb-1';
+		order--;
+		return false;
+	}*/
+
+	if (order==8) { // Hvis alle felter er udfyldt, altså der ikke er fratrukket fra order, eksekveres dette if-statement.
+		alert("Bestilling gennemført.");
+	}
+}
