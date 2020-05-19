7febcb08d83ddca753df4cf9894dc21378d00fe8
Malene Madsen <malene.t.madsen@hotmail.com>
2020-05-19 13:37:24 +0200

    Featherlight implementet

--- script.js
+++ script.js
@@ -43,19 +43,21 @@ function loopingIt(){
 
 
 
+$(document).ready(function() {
+    
+    // featherlight gallery configuration
+    $('a.gallery').featherlightGallery({
+		previousIcon: '&#9664;',     /* Code that is used as previous icon */
+		nextIcon: '&#9654;',         /* Code that is used as next icon */
+		galleryFadeIn: 100,          /* fadeIn speed when slide is loaded */
+		galleryFadeOut: 300          /* fadeOut speed before slide is loaded */
+	});
+    
+}) // <- end document.ready()
 
 
 
 
-function switchStyle() {
-  if (document.getElementById('styleSwitch').checked) {
-    document.getElementById('gallery').classList.add("custom");
-    document.getElementById('exampleModal').classList.add("custom");
-  } else {
-    document.getElementById('gallery').classList.remove("custom");
-    document.getElementById('exampleModal').classList.remove("custom");
-  }
-}
 
 
 
