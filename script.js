1289144736b4b28b6e6d4ab0e746744d75716c93
Sara Herlet <Sara.Herlet@gmail.com>
2020-05-19 16:38:13 +0200

    contact form in footer on index

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function loopingIt(){
 
 /*bestil*/
 function validate(){
-  var applicant=4; // Der kan max fratrækkes 8, derfor er denne værdi sat til 8
+  var applicant=5; // Der kan max fratrækkes 8, derfor er denne værdi sat til 8
   var name = document.getElementById("name").value;
   var email = document.getElementById("email").value;
   var instagram = document.getElementById("instagram").value;
@@ -95,7 +95,46 @@ function validate(){
 	return false; 
 	}
 
-	if (applicant==4) { // Hvis alle felter er udfyldt, altså der ikke er fratrukket fra order, eksekveres dette if-statement.
+	if (applicant==5) { // Hvis alle felter er udfyldt, altså der ikke er fratrukket fra order, eksekveres dette if-statement.
 		alert("Your application has been sent");
 	}
 }
+
+/*bestil*/
+function validatecontact(){
+  var applicant=3; // Der kan max fratrækkes 8, derfor er denne værdi sat til 8
+  var name = document.getElementById("contactname").value;
+  var email = document.getElementById("contactemail").value;
+  var msg = document.getElementById("messagecontact").value;
+  var error_message = document.getElementById("error_message");
+
+  var text;
+  if(name.length < 1){ // Dette if-statement tjekker om kæledyrsnavnet er mindre end 1
+    text = "Insert your name"; // sætter teksten til det valgte
+    error_message.innerHTML = text; // indsætter teksten i HTML
+	error_message.className = 'alert-danger mb-1'; // Den indsatte tekst bliver rød
+	applicant--; // mangler udfyldt felt medfører en fratrækning fra order.
+	return false; 
+	}
+	
+  if(email.indexOf("@") == -1 || email.length < 6){
+	text = "Insert your email";
+    error_message.innerHTML = text;
+	error_message.className = 'alert-danger mb-1';
+	applicant--;
+	return false;
+	}
+
+	if(msg.length < 1){ // Dette if-statement tjekker om kæledyrsnavnet er mindre end 1
+    text = "Insert some details about yourself"; // sætter teksten til det valgte
+    error_message.innerHTML = text; // indsætter teksten i HTML
+	error_message.className = 'alert-danger mb-1'; // Den indsatte tekst bliver rød
+	applicant--; // mangler udfyldt felt medfører en fratrækning fra order.
+	return false; 
+	}
+	
+	if (applicant==3) { // Hvis alle felter er udfyldt, altså der ikke er fratrukket fra order, eksekveres dette if-statement.
+		alert("Your application has been sent");
+	}
+}
+
