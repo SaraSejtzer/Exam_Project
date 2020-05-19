755eb49c1ae0cb7c4f3831c4f7d5f19f573e492d
Sara Herlet <Sara.Herlet@gmail.com>
2020-05-19 14:59:08 +0200

    removing featherlight and making space for different solution

--- script.js
+++ script.js
@@ -38,26 +38,6 @@ function loopingIt(){
 	loopingText(); //Calls the function loopingText which creates the loop. 
 }
 
-	
-
-
-
-
-$(document).ready(function() {
-    
-    // featherlight gallery configuration
-    $('a.gallery').featherlightGallery({
-		previousIcon: '&#9664;',     /* Code that is used as previous icon */
-		nextIcon: '&#9654;',         /* Code that is used as next icon */
-		galleryFadeIn: 100,          /* fadeIn speed when slide is loaded */
-		galleryFadeOut: 300          /* fadeOut speed before slide is loaded */
-	});
-    
-}) // <- end document.ready()
-
-
-
-
 
 
 
